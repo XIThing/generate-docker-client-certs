3c7c235e1443840a018004099c15b40404324e4f
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-11-26 18:49:31 +0100

    add mandatory whitespace before the closing test bracket
    add double-quotes around argument expansions
    
    refactor

--- generate-client-certs.sh
+++ generate-client-certs.sh
@@ -4,24 +4,27 @@
 # - $1 - Path to ca.pem
 # - $2 - Path to ca-key.pem
 
-if [ "$1" == "" ] || [ "$2" == ""]; then
+if [ "$1" = "" ] || [ "$2" = "" ]; then
     echo "Please provide paths to the ca.pem and ca-key.pem files."
-    exit
+    exit 2
 fi
 
 echo 01 > ca.srl
-cp $1 ca.pem
-cp $2 ca-key.pem
+cp "$1" ca.pem
+cp "$2" ca-key.pem
 
 openssl genrsa -out client-key.pem
 openssl req -new -key client-key.pem -out client.csr
 
-echo extendedKeyUsage = clientAuth > extfile.cnf
-openssl x509 -req -days 365 -in client.csr -CA ca.pem -CAkey ca-key.pem -out client-cert.pem -extfile extfile.cnf
+echo 'extendedKeyUsage = clientAuth' > extfile.cnf
+
+openssl x509 -req -days 365 \
+    -in client.csr \
+    -out client-cert.pem \
+    -CA ca.pem \
+    -CAkey ca-key.pem \
+    -extfile extfile.cnf
 
 # Cleanup
-rm ca.srl
-rm ca.pem
-rm ca-key.pem
-rm client.csr
-rm extfile.cnf
+rm ca.srl ca.pem ca-key.pem client.csr extfile.cnf
+
